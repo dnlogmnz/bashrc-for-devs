c3ecfcae4519ab661cd9a0646f4903b8574eda06
Danilo Gimenez <dnlogmnz@gmail.com>
2026-07-19 08:42:29 -0300

    feat: adicionar script de desativação de telemetria

---
+++ src/home/.config/bashrc/telemetry.sh
@@ -0,0 +1,14 @@
+#
+# Projeto: bashrc-for-devs
+#
+# Script: ~/.config/bashrc/telemetry.sh
+# Objetivo: desativar telemetria das CLIs de Nuvem no Terminal
+# =============================================================================
+
+export AWS_EXECUTION_ENV="CLI-No-Telemetry"
+export AZURE_CORE_COLLECT_TELEMETRY=0
+export CLOUDSDK_CORE_DISABLE_USAGE_REPORTING=true
+
+#--------------------------------------------------------------------------------
+#--- Final do script telemetry.sh
+#--------------------------------------------------------------------------------
